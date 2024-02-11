e9bc4fc3d814e13467b695d4c6f3840326460502
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-02-11 20:43:22 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,13 +1,12 @@
 def main():
-    name = get_name()
-    house = get_house()
+    name, house = get_student()
     print(f"{name} from {house}")
 
-def get_name():
-    return input("Name: ")
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return name, house
 
-def get_house():
-    return input("House: ")
 
 if __name__ == "__main__":
     main()
