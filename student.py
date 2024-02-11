3cd19c7409cf541ffcffb062e93491b375d0b2b9
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-02-11 20:45:58 +0530

    Update student.py

--- student.py
+++ student.py
@@ -5,8 +5,9 @@ def main():
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return name, house          # this is not returning 2 values rather it is returning one value in form of tuple
-
+    #return name, house          # this is not returning 2 values rather it is returning one value in form of tuple
+    #explicitly
+    return (name, house)
 
 if __name__ == "__main__":
     main()
