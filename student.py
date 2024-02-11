0261843a4a1b513e491283a1d8e7355482030b55
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-02-11 20:58:43 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,4 +1,4 @@
-#Tuple is similar as list in spirit but it is **immutable**.
+#Tuple is similar to list in spirit but it is **immutable**.
 
 def main():
     #name, house = get_student()        # no need to unpack if you are using a tuple explicitly 
@@ -8,7 +8,7 @@ def main():
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    #return name, house          # this is not returning 2 values rather it is returning one value in form of tuple
+    #return name, house          # This is not returning 2 values rather it is returning one value in the form of a tuple
     #explicitly
     return (name, house)
 
