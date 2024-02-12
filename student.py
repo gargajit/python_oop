f7a21799dcf1dd53ed1c0c2cfea9488b53a01739
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-02-12 19:55:03 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,4 +1,9 @@
-#Tuple is similar to list in spirit but it is **immutable**. Hence the values cannot be modified.
+# =========================================================================================
+# Using Tuple
+# ==========================================================================================
+
+'''
+# Tuple is similar to list in spirit but it is **immutable**. Hence the values cannot be modified.
 # If tried to modify, it will throw TypeError: 'tuple' object does not support item assignment
 
 def main():
@@ -9,9 +14,37 @@ def main():
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    #return name, house          # This is not returning 2 values rather it is returning one value in the form of a tuple
+    #return name, house      # This is not returning 2 values rather it is returning one value in the form of a tuple
     #explicitly
-    return (name, house)
+    #return (name, house)
+
+
+
+# So just in case, if by design you want to accept mutability and allow changes,
+# Instead of returning a (tuple) you can return a [list] 
+    
+    #return [name, house]
+    
+
+if __name__ == "__main__":
+    main()
+'''
+
+
+# ===============================================================================
+# Using Dictionary
+# ===============================================================================
+def main():
+    student = get_student()
+    print(f"{student['name']} from {student['house']}")
+
+
+def get_student():
+    student = {}
+    student['name'] = input("Name: ")
+    student['house'] = input("House: ")
+    return student
+
 
 if __name__ == "__main__":
     main()
