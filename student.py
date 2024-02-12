b65e7791e69a2f78f12703258ed566b11bc47f08
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-02-12 19:56:51 +0530

    Update student.py

--- student.py
+++ student.py
@@ -38,12 +38,17 @@ def main():
     student = get_student()
     print(f"{student['name']} from {student['house']}")
 
-
+'''
 def get_student():
     student = {}
     student['name'] = input("Name: ")
     student['house'] = input("House: ")
     return student
+'''
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return {'name': name, 'house': house}
 
 
 if __name__ == "__main__":
