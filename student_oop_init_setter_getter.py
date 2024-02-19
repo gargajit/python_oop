3205b9fa1a4431816a35bb0f3780f905e2d1a4e2
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-02-19 23:47:22 +0530

    Create student_oop_init_setter_getter.py

---
+++ student_oop_init_setter_getter.py
@@ -0,0 +1,34 @@
+class Student:
+    def __init__(self, name, house):
+        if not name:      # ie Pythonic way to say -> if name == ""
+            raise ValueError("Missing Name")
+        self.name = name
+        self.house = house
+        
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+    
+    @property
+    def house(self):
+       return self._house
+    
+    @house.getter
+    def house(self, house):
+       if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid House")
+       self._house = house
+    
+
+def main():
+  student = get_student()
+  print(student)
+
+
+def get_student():
+  name = input("Name: ")
+  house = input("House: ")
+  return Student(name, house)
+
+
+if __name__ == "__main__":
+  main()
