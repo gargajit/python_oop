cd98bea62ac466ad6cc92304c23be495477be2a1
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-03-31 20:35:07 +0530

    Update student_oop.py

--- student_oop.py
+++ student_oop.py
@@ -1,7 +1,8 @@
+'''
 class Student:
   ...            
 
-# that's enough code, lines 1&2 to just invent a new data type Student
+# that's enough code, lines 2&3 to just invent a new data type Student
 # that may or may not have some future functionlity as well.
 
 def main():
@@ -10,16 +11,33 @@ def main():
 
 def get_student():
   student = Student()    # Here we are creating an object* of the class Student
-  student.name = input("Name: ")
-  student.house = input("House: ")
+  student.name = input("Name: ")    #student.name is an attribute to the student variable
+  student.house = input("House: ")  #student.house is another attribute to the student variable
   return student
 
 if __name__ == "__main__":
   main()
 
 
-'''
+
 So we create **`objects from classes.`** 
 
 If `class` is a blueprint/mold then an `object` is when you use that blueprint to something specific.
 '''
+
+class Student:
+  def __init__(self, name, house):
+    self.name = name
+    self.house = house
+
+def main():
+  student = get_student()
+  print(f"{student.name} from {student.house}")
+
+def get_student():
+  name = input("Name: ")
+  house = input("House: ")
+  return Student(name, house)  # returning the return value of this Student() function that is constructing my new object
+
+if __name__ == "__main__":
+  main()
